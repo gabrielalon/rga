41355582c341f851928668de36e7f2357bf8d367
Marek Rode <m.rode@i-systems.pl>
2018-09-03 07:10:29 +0200

    [rga] Add modified at field #64013

---
+++ src/Domain/Model/Rga/Rga/ModifiedAt.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace RGA\Domain\Model\Rga\Rga;
+
+use RGA\Application\Assert\Assertion;
+
+final class ModifiedAt
+{
+	/** @var string */
+	private $date;
+	
+	/**
+	 * @param string $date
+	 * @return ModifiedAt
+	 */
+	public static function fromString(string $date): ModifiedAt
+	{
+		return new ModifiedAt($date);
+	}
+	
+	/**
+	 * @param string $date
+	 */
+	private function __construct(string $date)
+	{
+		Assertion::date($date, 'Y-m-d H:i:s', 'Invalid ModifiedAt string: ' . $date);
+		
+		$this->date = $date;
+	}
+	
+	/**
+	 * @return string
+	 */
+	public function toString(): string
+	{
+		return $this->date;
+	}
+	
+	/**
+	 * @param ModifiedAt $other
+	 * @return bool
+	 */
+	public function equals($other): bool
+	{
+		if (false === $other instanceof ModifiedAt)
+		{
+			return false;
+		}
+		
+		return $this->date === $other->date;
+	}
+	
+	/**
+	 * @return string
+	 */
+	public function __toString(): string
+	{
+		return $this->date;
+	}
+}
