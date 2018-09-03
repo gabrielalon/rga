5c7a9eb64967674087fbcd9c2a8fccadbccd7e9b
Marek Rode <m.rode@i-systems.pl>
2018-09-03 09:42:37 +0200

    [rga] fix standard service #64013

--- src/Infrastructure/Source/Service/StandardService.php
+++ src/Infrastructure/Source/Service/StandardService.php
@@ -47,10 +47,7 @@ class StandardService
 		$rmaObjectItemBuilder = new RgaObjectItemBuilder(
 			$itemId,
 			0,
-			'',
-			$this->sourceType(),
-			0,
-			null
+			''
 		);
 		
 		return $rmaObjectItemBuilder->build();
