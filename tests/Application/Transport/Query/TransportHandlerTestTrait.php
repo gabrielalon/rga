d61bb7c208fd64aab2609536c9de1f27e8caf54a
Marek Rode <m.rode@i-systems.pl>
2019-04-12 09:34:27 +0200

    [rga] add baser #68992

--- tests/Application/Transport/Query/TransportHandlerTestTrait.php
+++ tests/Application/Transport/Query/TransportHandlerTestTrait.php
@@ -4,17 +4,20 @@
 namespace RGA\Test\Application\Transport\Query;
 
 use RGA\Application\Transport\Query\ReadModel\Transport;
+use RGA\Domain\Model\Transport\Transport\Active;
 
 trait TransportHandlerTestTrait
 {
 	/**
 	 * @param string $uuid
 	 * @param string $domain
+	 * @param bool $active
 	 * @return Transport
 	 */
-	protected function createTransportView(string $uuid, string $domain): Transport
+	protected function createTransportView(string $uuid, string $domain, bool $active = true): Transport
 	{
 		return Transport::fromUuid($uuid)
+			->setActive(Active::fromBoolean($active))
 			->addDomain($domain)
 		;
 	}
